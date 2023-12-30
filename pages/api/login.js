8df0ec9d2475a16b782e15977f94f5e8ae210222
Mahir Abrar <hamid.mahir2597@Yahoo.com>
2023-12-30 21:25:16 +0600

    Successfully integrated passport js for auth

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,4 +1,7 @@
 import connectToDB from "@/app/database";
+import passport from "passport";
+import "@/app/auth/passport-config";
+
 import User from "@/app/models/user";
 import { compare } from "bcryptjs";
 import Joi from "joi";
@@ -9,72 +12,49 @@ const schema = Joi.object({
   password: Joi.string().required(),
 });
 
-export default async function handler(req, res) {
+export default function handler(req, res, next) {
   console.log("Running api/login");
-  await connectToDB();
-
-  const { email, password } = req.body;
-  const { error } = schema.validate({ email, password });
-  let correctEmail = email.toLowerCase();
-
-  console.log("validating email");
-  if (error) {
-    return res.status(400).json({
-      success: false,
-      message: `Not correct input form for email and password ${error.details[0].message}`,
-    });
-  }
-  console.log("schema Validated");
-
-  try {
-    const user = await User.findOne({ correctEmail });
-    if (!user) {
-      return res.status(400).json({
+  connectToDB()
+    .then(() => {
+      passport.authenticate("local", (err, user, info) => {
+        if (err) {
+          return next(err);
+        }
+        if (!user) {
+          return res.status(400).json({
+            success: false,
+            message: info.message || "Incorrect email or password",
+          });
+        }
+        req.logIn(user, { session: false }, async (err) => {
+          if (err) {
+            return next(err);
+          }
+          // Generate JWT or any other post-login logic here
+          const token = jwt.sign(
+            { id: user._id, email: user.email, role: user.role },
+            "default_secret_key",
+            { expiresIn: "1d" },
+          );
+          return res.status(200).json({
+            success: true,
+            message: "Login successful!",
+            token,
+            user: {
+              email: user.email,
+              name: user.name,
+              _id: user._id,
+              role: user.role,
+            },
+          });
+        });
+      })(req, res, next);
+    })
+    .catch((e) => {
+      console.error("Database connection error", e);
+      res.status(500).json({
         success: false,
-        message: "Account not found",
+        message: "Internal server error",
       });
-    }
-    console.log("Email found Validated");
-    const checkPassword = await compare(password, user.password);
-    if (!checkPassword) {
-      return res.status(400).json({
-        success: false,
-        message: "Incorrect email or password",
-      });
-    }
-    console.log("Password Validated");
-
-    const token = jwt.sign(
-      {
-        id: user._id,
-        email: user?.correctEmail,
-        role: user?.role,
-      },
-      "default_secret_key",
-      { expiresIn: "1d" },
-    );
-
-    const finalData = {
-      token,
-      user: {
-        email: user.correctEmail,
-        name: user.name,
-        _id: user._id,
-        role: user.role,
-      },
-    };
-
-    return res.status(200).json({
-      success: true,
-      message: "Login successful!",
-      finalData,
     });
-  } catch (e) {
-    console.log("Error in api/route/login", e);
-
-    return res.status(500).json({
-      success: false,
-      message: "api/route/login",
-    });
-  }
 }
---
+++ src/app/auth/passport-config.js
@@ -0,0 +1,37 @@
+// src/app/auth/passport-config.js
+import passport from "passport";
+import LocalStrategy from "passport-local";
+import bcrypt from "bcryptjs";
+import User from "../models/user";
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+    },
+    async (email, password, done) => {
+      try {
+        const user = await User.findOne({ email });
+        if (!user || !(await bcrypt.compare(password, user.password))) {
+          return done(null, false, { message: "Incorrect email or password." });
+        }
+        return done(null, user);
+      } catch (err) {
+        return done(err);
+      }
+    },
+  ),
+);
+
+passport.serializeUser((user, done) => done(null, user.id));
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+export default passport;
